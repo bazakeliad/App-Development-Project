c36c404f5df7f5b818cdcf77e30998ebd3457558
itay55 <shabo.itay10@gmail.com>
2024-10-17 02:58:05 +0300

    rest api fixes

--- public/js/adminJerseys.js
+++ public/js/adminJerseys.js
@@ -1,7 +1,7 @@
 // Function to send DELETE request using Ajax by JQUERY
 async function deleteResource(jerseyId) {
     $.ajax({
-        url: `/admin/jerseys/delete/${jerseyId}`,
+        url: `/jerseys/${jerseyId}`,
         type: 'DELETE',
         success: function(result) {
             // On success, remove the item from the DOM
