a432ed72bbbd1f0a38c50f6dd26d4372934e8664
Ben Epstein <benepstein@Bens-MacBook-Pro-14.local>
2024-11-01 15:39:59 +0200

    Fix Login container size and move script tag from ejs

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,8 @@
-// if (location.href.endsWith("?error=1")) {
-//     document.body.innerHTML += "<b>Username and/or password is incorrect!</b>";
-// }
-// if (location.href.endsWith("?error=2")) {
-//     document.body.innerHTML += "<b>Something went wrong with the registration!</b>";
-// } 
 document.addEventListener("DOMContentLoaded", function () {
     const loginErrorDiv = document.getElementById("loginError");
     const registerErrorDiv = document.getElementById("registerError");
 
+    // Handle URL errors
     if (location.href.endsWith("?error=1")) {
         loginErrorDiv.innerHTML = "Username and/or password is incorrect!";
     }
@@ -16,4 +11,18 @@ document.addEventListener("DOMContentLoaded", function () {
         $('#signup-tab').click();
         registerErrorDiv.innerHTML = "Something went wrong with the registration!";
     }
+
+    // Password Confirmation Check
+    document.getElementById('signupForm').addEventListener('submit', function(event) {
+        const password = document.getElementById('signupPassword').value;
+        const confirmPassword = document.getElementById('signupConfirmPassword').value;
+        const passwordError = document.getElementById('passwordError');
+
+        if (password !== confirmPassword) {
+            event.preventDefault();
+            passwordError.textContent = "Passwords do not match. Please re-enter.";
+        } else {
+            passwordError.textContent = ""; // Clear any previous error messages
+        }
+    });
 });
